5b76d9cb667b35130d7d4d415b9ad53f87775334
NikitaRybakov21 <nikitarybakov044@gmail.com>
2021-07-23 20:30:02 +0300

    DZ10

--- src/main/java/github/Main.java
+++ src/main/java/github/Main.java
@@ -3,5 +3,19 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        String[] words = {"Банан", "Арбуз","Огурец","Помидор","Баклажан",
+                          "Арбуз","Банан","Картошка","Сливы","Банан","Перец"
+        };
+
+        Set<String> set = new HashSet<>();
+        for (String s : words) {
+            int sum = 0;
+            for (String word : words) {
+                if (s.equals(word)) sum++;
+            }
+            set.add(s + " " + sum + " раз");
+        }
+        System.out.println(set);
+    }
 }
--- src/main/java/github/PhoneDirectory.java
+++ src/main/java/github/PhoneDirectory.java
@@ -3,5 +3,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PhoneDirectory {
+    static Map<String,Long> map = new HashMap<>();
 
+    public static void main(String[] args) {
+        directory();
+    }
+    public static void directory(){
+        add("Рома",89992561122L);
+        add("Валера",88005553535L);
+        add("Петя",123456789123L);
+        add("Рома",456535152545L);
+    }
+    public static void add(String name,long nam) {
+       map.put(name,nam);
+
+    }
 }
